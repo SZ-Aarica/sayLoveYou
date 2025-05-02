75e83055fc28912990120d481acc3f25b942f661
SZ-Aarica <112103155+SZ-Aarica@users.noreply.github.com>
2025-05-02 16:54:55 +0330

    working for a next rounds

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function displayIloveYou(response){
-      let showBox = document.querySelector("#loader");
-    showBox.classList.remove("loader");
+     //const div = document.getElementById("IloveYou");
+   // const loader = document.getElementById("loader");
+    // Hide loader
+    //loader.classList.add("hidden");
+    
  new Typewriter('#IloveYou', {
     strings: response.data.answer,
     autoStart: true,
@@ -10,16 +13,26 @@ function displayIloveYou(response){
 }
 function generate(event){
     event.preventDefault();
+     let ShowTextBox = document.querySelector("#IloveYou");
+
+    if(ShowTextBox.classList.contains("hidden")){
+        ShowTextBox.classList.remove("hidden");
+    }else{
+        ShowTextBox.innerHTML = "<div class='nothing' id='loader'></div>";
+
+    }
     let instructionInput=document.querySelector("#userInstruction");
-    //console.log(instructionInput.value);
+
+    
     let apiKey = "c1f9c61a43fbf0460to00d7010dfa109";
     let prompt = "you are a language expert. please provide a short anwer and only say i love you in the given language";
     let context =`what is it in ${instructionInput.value}`;
     let Url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    let showBox = document.querySelector("#loader");
-    showBox.classList.add("loader");
-
+    let loader = document.querySelector("#loader");
+    if(!loader.classList.contains(loader)){
+       loader.classList.add("loader");
+    }
     
     axios.get(Url).then(displayIloveYou);
    
